2169a0ada48c170d948877a323644635cbd34089
Simon Roth <systats@users.noreply.github.com>
2017-11-03 02:10:13 +0100

    New

---
+++ R/searcher.R
@@ -0,0 +1,45 @@
+#' Find and SPSS variable names
+#'
+#' @export
+
+var_names <- function(data, keyword = "") {
+  keyword <- ifelse(keyword %in% "all", "", keyword)
+  #if 'all' turn into void, else copy keyword
+  lablist <-  data %>%
+    var_label() %>% # extract variable labels
+    bind_rows() %>% # binding list elements as dataframe
+    t() # transpose dataframe
+  name_pos <- stringr::str_detect(tolower(lablist[, 1]), tolower(keyword))
+  # get position of string
+  if(any(name_pos)){ #if the string is found
+    dat <-data.frame(var_codes=names(lablist[name_pos, ]),
+                     var_names=lablist[name_pos, ],
+                     row.names = NULL, stringsAsFactors = F)
+    #colnames(dat) <- "var_names"
+    cat(paste0("####---- Nice! You found ", nrow(dat) , " variables! ----#### \n \n "))
+    return(dat)
+  } else{
+    cat("Variable Name not found. Try again, Stupid!")
+  }
+}
+
+
+searcher <- function(data) {
+
+  ui <- miniPage(
+    gadgetTitleBar("searcher"),
+    miniContentPanel(
+      DT::dataTableOutput("tab")
+    )
+  )
+
+  server <- function(input, output, session) {
+
+    # Render the plot
+    output$tab <- DT::renderDataTable({
+      return(var_names(data, ""))
+    })
+  }
+
+  runGadget(ui, server, viewer = dialogViewer("ggbrush", width = 900, height = 600))
+}
