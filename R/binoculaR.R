87494f5fa949ac4a7f23aa21e8c01f312810a9b6
favstats <fabio.votta@gmail.com>
2017-12-18 15:43:44 +0100

    fixed emply lists problem

--- R/binoculaR.R
+++ R/binoculaR.R
@@ -11,11 +11,16 @@ binoculaR <- function(data) {
   library(dplyr)
   library(DT)
 
+  remove_nulls  <-  function(list){   # delele null/empty entries in a list
+    list[unlist(lapply(list, length) != 0)]
+  }
+
   var_names <- function(data, keyword = "") {
     keyword <- ifelse(keyword %in% "all", "", keyword)
     #if 'all' turn into void, else copy keyword
     lablist <-  data %>%
       var_label() %>% # extract variable labels
+      remove_nulls() %>% #remove empty lists
       dplyr::bind_rows() %>% # binding list elements as dataframe
       t() # transpose dataframe
     name_pos <- stringr::str_detect(tolower(lablist[, 1]), tolower(keyword))
@@ -28,23 +33,26 @@ binoculaR <- function(data) {
       cat(paste0("####---- Nice! You found ", nrow(dat) , " variables! ----#### \n \n "))
       return(dat)
     } else{
-      cat("Variable Name not found. Try again, Stupid!")
+      cat("Variable Name not found. Try again with a different name.")
     }
   }
+
+
+
 
 
   ui <- miniPage(
     gadgetTitleBar("binoculaR (Roth & Votta)"),
       miniTabstripPanel(
         miniTabPanel(
-          "Full Dataset", 
+          "Full Dataset",
           icon = icon("table"),
           miniContentPanel(
             DT::dataTableOutput("tab")
           )
         ),
         miniTabPanel(
-          "Selected", 
+          "Selected",
           icon = icon("sliders"),
           miniContentPanel(
             DT::dataTableOutput("selected")
@@ -60,7 +68,7 @@ binoculaR <- function(data) {
     output$tab <- DT::renderDataTable({
       return(dat())
     })
-    
+
     output$selected <- DT::renderDataTable({
       req(input$tab_rows_selected)
       return(dat()[input$tab_rows_selected, ])
